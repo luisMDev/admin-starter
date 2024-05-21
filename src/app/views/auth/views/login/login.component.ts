11c8acddfc4176ba70fff97b58b463a91bb4d9b5
Luis M Torres <luis.torres@dixper.gg>
2024-05-21 10:24:34 +0200

    fix: switch to login component from login-admin

--- src/app/views/auth/views/login/login.component.ts
+++ src/app/views/auth/views/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { ArcLoginAdminComponent } from '@arcane-auth/ngx-client';
+import { ArcLoginComponent } from '@arcane-auth/ngx-client';
 import { ThemeSwitcherComponent } from '../../../../shared/components/theme-switcher/theme-switcher.component';
 
 @Component({
   selector: 'admin-starter-login',
   templateUrl: './login.component.html',
   standalone: true,
-  imports: [ArcLoginAdminComponent, ThemeSwitcherComponent],
+  imports: [ArcLoginComponent, ThemeSwitcherComponent],
 })
 export class LoginComponent {}
